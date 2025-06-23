ff0faf63684b29d0c088f178fc729cc317a9045d
Vicfou <victormorel1998@gmail.com>
2025-06-23 22:55:26 +0200

    push

--- dump.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-$process = Start-Process -FilePath ".\Il2CppDumper\Il2CppDumper.exe" -ArgumentList ".\parser\temp\GameAssembly.dll", ".\parser\temp\Dofus_Data\il2cpp_data\Metadata\global-metadata.dat" -PassThru -NoNewWindow
-Start-Sleep -Seconds 30
-[System.Windows.Forms.SendKeys]::SendWait("`n")
-$process.WaitForExit()
--- parser/src/utils.ts
+++
@@ -1,7 +0,0 @@
-import fs from "fs/promises";
-
-export async function createFoldersRecursively(folder: string) {
-  try {
-    await fs.mkdir(folder, { recursive: true });
-  } catch {}
-}
